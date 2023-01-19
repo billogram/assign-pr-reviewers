fd1d6dce7dc9cbf278de6859185ea9a913cde7e3
nils.hermansson <nils.hermansson@billogram.com>
2023-01-19 10:55:28 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ import { Context } from '@actions/github/lib/context';
               format: 'diff'
             }
         });
-        core.info(`XXX: ${pullRequest.requested_reviewers}`);
+        core.info(`XXX: ${pullRequest}`);
 
         if (!token) {
             return core.setFailed(`Required input "token" not provided`);
